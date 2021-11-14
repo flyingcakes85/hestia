08249b7f9dd91ae3b5abc657b06960385aea1718
Snehit Sah <snehitsah@protonmail.com>
2021-11-14 12:30:20 +0530

    Wrap main in safearea
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -29,97 +29,99 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.settings),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.warning),
-      ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 300,
-            width: double.infinity,
-            child: Column(
-              children: const [
-                SizedBox(
-                  height: 50,
-                ),
-                CircleAvatar(
-                  radius: 80,
-                  backgroundImage: NetworkImage("https://picsum.photos/400"),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "Sabrina Ken",
-                  style: TextStyle(
-                      fontSize: 35,
-                      fontWeight: FontWeight.w200,
-                      color: Colors.white),
-                ),
-              ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.settings),
             ),
-          ),
-          Expanded(
-            child: ListView(
-              children: [
-                const SizedBox(
-                  height: 10,
-                ),
-                const HomepageCard(
-                  title: "User Info",
-                  subTitle: "All info is filled. You're good to go!",
-                  icon: Icons.check_circle_outline,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                InkWell(
-                  child: const HomepageCard(
-                    title: "Emergency Numbers",
-                    subTitle: "Show and call on emergency numbers.",
-                    icon: Icons.call,
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.warning),
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Column(
+                children: const [
+                  SizedBox(
+                    height: 50,
                   ),
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const EmergencyNumbers()));
-                  },
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const HomepageCard(
-                  title: "Share live location",
-                  subTitle: "Share your location that updates in real time.",
-                  icon: Icons.location_on,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const HomepageCard(
-                  title: "Travel Mode",
-                  subTitle:
-                      "Your contacts will be automatically alerted if you don't return.",
-                  icon: Icons.card_travel,
-                ),
-                const SizedBox(
-                  height: 80,
-                ),
-              ],
+                  CircleAvatar(
+                    radius: 80,
+                    backgroundImage: NetworkImage("https://picsum.photos/400"),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Sabrina Ken",
+                    style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.w200,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            Expanded(
+              child: ListView(
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const HomepageCard(
+                    title: "User Info",
+                    subTitle: "All info is filled. You're good to go!",
+                    icon: Icons.check_circle_outline,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  InkWell(
+                    child: const HomepageCard(
+                      title: "Emergency Numbers",
+                      subTitle: "Show and call on emergency numbers.",
+                      icon: Icons.call,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const EmergencyNumbers()));
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const HomepageCard(
+                    title: "Share live location",
+                    subTitle: "Share your location that updates in real time.",
+                    icon: Icons.location_on,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const HomepageCard(
+                    title: "Travel Mode",
+                    subTitle:
+                        "Your contacts will be automatically alerted if you don't return.",
+                    icon: Icons.card_travel,
+                  ),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
